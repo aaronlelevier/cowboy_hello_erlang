95d424aba0088b2083f48a3eadc1a0afc42bb0eb
aaronlelevier <aaron.lelevier@gmail.com>
2019-09-14 12:42:42 -0700

    add example for url other than home page url

--- ebin/hello_erlang.app
+++ ebin/hello_erlang.app
@@ -1,7 +1,7 @@
 {application, 'hello_erlang', [
 	{description, "New project"},
 	{vsn, "0.1.0"},
-	{modules, ['hello_erlang_app','hello_erlang_sup','hello_handler']},
+	{modules, ['blog_handler','hello_erlang_app','hello_erlang_sup','hello_handler']},
 	{registered, [hello_erlang_sup]},
 	{applications, [kernel,stdlib,cowboy]},
 	{mod, {hello_erlang_app, []}},
--- src/hello_erlang_app.erl
+++ src/hello_erlang_app.erl
@@ -5,16 +5,21 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	% Routing docs: https://ninenines.eu/docs/en/cowboy/2.6/guide/routing/
-	% {HostMatch, list({PathMatch, Handler, InitialState})}
-	Dispatch = cowboy_router:compile([
-		{'_', [{"/", hello_handler, #{}}]}
-	]),
-	{ok, _} = cowboy:start_clear(my_http_listener,
-		[{port, 8080}],
-		#{env => #{dispatch => Dispatch}}
-	),
-	hello_erlang_sup:start_link().
+  % Routing docs: https://ninenines.eu/docs/en/cowboy/2.6/guide/routing/
+  % {HostMatch, list({PathMatch, Handler, InitialState})}
+  Dispatch = cowboy_router:compile([
+    {'_', [
+			% path to resource example
+			{"/blog", blog_handler, #{}},
+			% home page
+      {"/", hello_handler, #{}}
+    ]}
+  ]),
+  {ok, _} = cowboy:start_clear(my_http_listener,
+    [{port, 8080}],
+    #{env => #{dispatch => Dispatch}}
+  ),
+  hello_erlang_sup:start_link().
 
 stop(_State) ->
-	ok.
+  ok.
